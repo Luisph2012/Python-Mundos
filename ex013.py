ac2a843a768bd57f48acd8b3ce0e7eec394143d7
Luis Philipe Vilara Ribeiro <89031690+Luisph2012@users.noreply.github.com>
2021-10-27 15:04:52 -0300

    Knowledge

--- ex013.py
+++ ex013.py
@@ -1,1 +1,7 @@
-asdasdasdasdasdasdasd
+###Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+
+salarioAtual = float(input('Fale o seu salário atual: R$'))
+
+salarioAumento = salarioAtual* 1.15
+
+print('O seu novo salário (com Aumento) é de {:.2f}!'.format(salarioAumento))
